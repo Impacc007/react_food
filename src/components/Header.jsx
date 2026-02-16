6ecab6e180ebaec80d40fdbbd86511ea7369c7e7
Leon Tran <leontran@Leons-Mac-mini.local>
2026-02-16 23:47:57 +0700

    Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,8 +2,8 @@ import logoImg from "../assets/logo.jpg";
 
 export default function Header() {
   return (
-    <header>
-      <div>
+    <header id="main-header">
+      <div id="title">
         <img src={logoImg} alt="Restaurant img" />
         <h1>Reactfood</h1>
       </div>
